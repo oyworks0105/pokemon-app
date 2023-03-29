9a1190592dcc2b8d25aaa95078e634f37b7b7bd4
oyworks0105 <97151590+oyworks0105@users.noreply.github.com>
2023-03-29 16:54:53 +0900

    Add files via upload
    test

---
+++ App.js
@@ -0,0 +1,84 @@
+import { useEffect, useState } from 'react';
+import { getAllPokemon, getPokemon } from './utils/pokemon.js';
+import './App.css';
+import Card from './components/Card/Card.js';
+import Navbar from './components/Navbar/Navbar.js';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextURL, setNextURL] = useState("");
+  const [prevURL, setPrevURL] = useState("");
+
+  useEffect(() => {
+  const fetchPokemonData = async () => {
+    //全てのポケモンデータを取得
+    let res = await getAllPokemon(initialURL);
+    //各ポケモンの詳細なデータを取得
+    loadPokemon(res.results);
+    // console.log(res);
+    setNextURL(res.next);
+    setPrevURL(res.previous); //null
+    setLoading(false);
+  }
+  fetchPokemonData();
+  }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        // console.log(pokemon);
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  // console.log(pokemonData);
+  const handleNextPage = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextURL);
+    await loadPokemon(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+  const handlePrevPage = async () => {
+    if(!prevURL) return;
+
+    setLoading(true);
+    let data = await getAllPokemon(prevURL);
+    await loadPokemon(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setLoading(false);
+  };
+
+  return (
+    <>
+    <Navbar />
+      <div className="App">
+
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <>
+          <div className='pokemonCardcontainer'>
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />;
+            })}
+          </div>
+          <div className="btn">
+            <button onClick={handlePrevPage}>前へ</button>
+            <button onClick={handleNextPage}>次へ</button>
+          </div>
+        </>
+      )}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOcwpzIgzaurN1JWOuJ7aQ71BF1NDIwq0",
+  authDomain: "pokemon-app-ba69a.firebaseapp.com",
+  projectId: "pokemon-app-ba69a",
+  storageBucket: "pokemon-app-ba69a.appspot.com",
+  messagingSenderId: "886222288406",
+  appId: "1:886222288406:web:03780c2edaf20c269fa737"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
